09a055ceef6eb97c89023fcd04ee566eb37bd247
Lucas Anjos <112708978+lucasanjos03@users.noreply.github.com>
2025-12-29 23:54:43 -0300

    fix: saida de dados

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,10 +1,25 @@
-import java.util.Locale;
-
-public class Main {
+public class Main
+{
     public static void main(String[] args) {
+        String product1 = "computador";
+        String product2 = "Mesa de escritorio";
 
-        double x = 34.8768;
-        Locale.setDefault(Locale.US);
-        System.out.printf("%.2f%n", x);
+        int age = 30;
+        int code = 5290;
+        char gender = 'f';
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+
+        System.out.println("Produtos:");
+        System.out.printf("Computador, está custando $ %.2f%n", price1);
+        System.out.printf("Mesa de escritorio, está custando $ %.2f%n", price2);
+
+        System.out.printf("Record: idade de %d, código %d e genero é: %c%n", age, code, gender);
+
+        System.out.printf("Medida com oito casas decimais: %.8f%n", measure);
+        System.out.printf("Arredondando, com três casas decimais: %.3f%n", measure);
+        //string S, int D, double f, char c
     }
 }
